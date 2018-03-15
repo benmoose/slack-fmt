0ec930644257f4652e72f7be824c40c7136a58fd
Ben Hadfield <benjohnhadfield@gmail.com>
2018-03-15 16:15:50 +0000

    args parsed, router refactor

---
+++ src/controller/help/index.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+
+import { IFormatter } from '../../common/types'
+import { createMessage, formatAsCodeBlock, parseMessageArgs } from '../../utils/msg'
+
+const format = (req: Request, res: Response) => {
+  const helpText = `*To format a string* type \`/fmt [spaces] json\`.
+\t\`spaces\` _(optional, default 2)_
+\t\tnumber of spaces to indent by. If 0 then the message is not formatted
+\t\`json\`
+\t\tjson string to format (must be valid JSON).
+  `
+  const message = createMessage(helpText, {
+    ephemeral: true
+  })
+  res.json(message)
+}
+
+const help: IFormatter = { format }
+
+export default help
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,7 +1,9 @@
 import { IController } from '../common/types'
+import help from './help'
 import json from './json'
 
 const controller: IController = {
+  help,
   json
 }
 
