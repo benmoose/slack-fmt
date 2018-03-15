ebc834e22c5192efcc236bcd957914e7062e3776
Ben Hadfield <benjohnhadfield@gmail.com>
2018-03-15 22:25:15 +0000

    direct install link

--- src/routes/oauth/index.ts
+++ src/routes/oauth/index.ts
@@ -3,5 +3,6 @@ import controller from '../../controller'
 
 const router = Router()
 router.get('/redirect', controller.oauth.redirect)
+router.get('/direct-install', controller.oauth.directInstall)
 
 export default router
