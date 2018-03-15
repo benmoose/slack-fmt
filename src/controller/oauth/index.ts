948b1eed70c9eb31451242c895712fc9975ec91e
Ben Hadfield <benjohnhadfield@gmail.com>
2018-03-15 22:25:15 +0000

    direct install link

--- src/controller/oauth/index.ts
+++ src/controller/oauth/index.ts
@@ -1,31 +1,36 @@
+import { URL, URLSearchParams } from 'url'
 import axios from 'axios'
 import { Request, Response } from 'express'
 
 const redirect = (req: Request, res: Response): Response => {
   const code: string = req.query.code
-  const state: string = req.query.state
-  const expectedState = 'commands'
   // check state matches and a code was provided
-  if (state !== expectedState || !code) {
-    return res.status(400).send()
+  if (!code) {
+    return res.status(400).json({
+      message: 'Missing \'code\' parameter'
+    })
   }
+  // prepare data to call Slack's API
   const data = {
     code,
     client_id: process.env.SLACK_CLIENT_ID,
     client_secret: process.env.SLACK_SECRET
   }
   // call Slack oauth.access API
-  axios.post(process.env.SLACK_OAUTH_ACCESS_URL, data)
+  axios.post(process.env.SLACK_OAUTH_ACCESS_URL, data, {
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+    }
+  })
     .then((response) => {
       const { data, status } = response
       if (status === 200) {
         // slack sent us a response...
         const accessToken = data.access_token
-        console.log(data)
         // check response
         if (data.ok && data.access_token) {
           // success!
-          return res.status(200).send()
+          return res.status(200).json({ message: 'App installed!' })
         }
       }
       return res.status(400).json({
@@ -34,4 +39,21 @@ const redirect = (req: Request, res: Response): Response => {
     })
 }
 
-export default { redirect }
+/**
+ * Direct install endpoint
+ * https://api.slack.com/slack-apps#direct_install
+ * @param req
+ * @param res
+ */
+const directInstall = (req: Request, res: Response): void => {
+  // build authorisation URL
+  const authoriseUrl = new URL(process.env.SLACK_OAUTH_AUTHORISATION_PAGE)
+  authoriseUrl.search = new URLSearchParams({
+    client_id: process.env.SLACK_CLIENT_ID,
+    scope: 'commands'
+  }).toString()
+  // 302 to authorise endpoint
+  return res.redirect(authoriseUrl.href)
+}
+
+export default { redirect, directInstall }
