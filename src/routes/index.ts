ebc48329c5b898b7f4c5b5f2d90e30706066aa68
Ben Hadfield <benjohnhadfield@gmail.com>
2018-03-15 17:01:47 +0000

    verification token check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-// import controller from '../controller'
+
+import { checkVerificationToken } from '../utils/request'
 import dispatcher from './dispatch'
 
 const router = Router()
 
+router.use(checkVerificationToken)
 router.post('/', dispatcher)
 
 export default router
--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,11 +1,18 @@
-import { Request } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { CommandType } from '../../common/types'
 
 /**
  * Takes a request and returns a CommandType.
  */
-export const getRequestCommandType = (req: Request): CommandType => {
+export function getRequestCommandType (req: Request): CommandType {
   const text = req.body.text
   const isHelpCommand = /^help(?!\S)/i.test(text)
   return isHelpCommand ? CommandType.Help : CommandType.Format
+}
+
+export function checkVerificationToken (req: Request, res: Response, next: NextFunction): Response {
+  if (req.body.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+    return res.status(403).send()
+  }
+  next()
 }
