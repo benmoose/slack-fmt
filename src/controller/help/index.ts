7b5fead52f200091558793b1d804f37846b1d5a0
Ben Hadfield <benjohnhadfield@gmail.com>
2018-03-25 14:57:40 +0100

    update help section to use argparse

--- src/controller/help/index.ts
+++ src/controller/help/index.ts
@@ -1,19 +1,12 @@
 import { Request, Response } from 'express'
 
 import { IFormatter } from '../../common/types'
-import { createMessage, formatAsCodeBlock, parseMessageArgs } from '../../utils/msg'
+import { createMessage } from '../../utils/msg'
+import { createArgumentParser } from '../../utils/parse'
 
 const format = (req: Request, res: Response): Response => {
-  // help message
-  const helpText = `*To format a string* type \`/fmt [spaces] json\`.
-\t\`spaces\` _(optional, default 2)_
-\t\tnumber of spaces to indent by. If 0 then the message is not formatted
-\t\`json\`
-\t\tjson string to format, either strict or relaxed JSON
-  `
-  const message = createMessage(helpText, {
-    ephemeral: true
-  })
+  const parser = createArgumentParser()
+  const message = createMessage(parser.formatHelp())
   return res.json(message)
 }
 
