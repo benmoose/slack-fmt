1f1dfebf295c563762c1a8486649a5597a041703
Ben Hadfield <benjohnhadfield@gmail.com>
2018-09-23 17:29:46 +0100

    update configs

---
+++ src/functions/format.ts
@@ -0,0 +1,54 @@
+import { prepareString } from '../utils/string'
+import { parseJsonString, parseMessageArgs } from '../utils/parse'
+import { createMessage, formatAsCodeBlock, isHelpCommand } from '../utils/msg'
+
+/**
+ * Format JSON from event body
+ */
+const formatJson = (body) => {
+  // parse arguments from the incoming string
+  const args = parseMessageArgs(body.text)
+  if (isHelpCommand(args.text)) {
+    return helpMessage
+  }
+  // sanitise text to format
+  const text = prepareString(args.text)
+  try {
+    // try to parse the JSON
+    const parsed = parseJsonString(text)
+    // format the parsed JSON with tabs
+    const formattedJson = JSON.stringify(parsed, null, ' '.repeat(args.spaces))
+    // wrap in markdown codeblock characters
+    const formattedText = formatAsCodeBlock(formattedJson)
+    // get the message object to send to Slack
+    return createMessage(formattedText, { ephemeral: false })
+  } catch {
+    // Parsing error, send back an error message to the user
+    return createMessage('There was a syntax error in the JSON :cry:', {
+      ephemeral: true
+    })
+  }
+}
+
+const helpMessage = createMessage(
+  `*To format a string* type \`/fmt [spaces] json\`.
+\t\`spaces\` _(optional, default 2)_
+\t\tnumber of spaces to indent by. If 0 then the message is not formatted
+\t\`json\`
+\t\tjson string to format, either strict or relaxed JSON`,
+  {ephemeral: true},
+)
+
+/**
+ * const helpText = `*To format a string* type \`/fmt [spaces] json\`.
+\t\`spaces\` _(optional, default 2)_
+\t\tnumber of spaces to indent by. If 0 then the message is not formatted
+\t\`json\`
+\t\tjson string to format, either strict or relaxed JSON
+  `
+  const message = createMessage(helpText, {
+    ephemeral: true
+  })
+ */
+
+export default formatJson
---
+++ src/handler.ts
@@ -0,0 +1,10 @@
+const querystring = require('querystring')
+import formatJson from './functions/format'
+
+export const slashCommand = async (event, context) => {
+    const body = querystring.parse(event.body)
+    return {
+        statusCode: 200,
+        body: JSON.stringify(formatJson(body))
+    }
+}
