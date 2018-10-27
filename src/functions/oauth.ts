39f79372222bf4ecacbddfb098e2523208a32644
Ben Hadfield <benjohnhadfield@gmail.com>
2018-10-27 14:57:23 +0100

    add full oauth flow

--- src/functions/oauth.ts
+++ src/functions/oauth.ts
@@ -1,10 +1,38 @@
-import { URL } from 'url'
+import axios from 'axios'
 import { stringify } from 'qs'
+import { URL } from 'url'
 
 export const getDirectInstallUrl = () => {
-    const qs = stringify({
-        client_id: process.env.SLACK_CLIENT_ID,
-        scope: 'commands'
+  const qs = stringify({
+    client_id: process.env.SLACK_CLIENT_ID,
+    scope: 'commands',
+    redirect_uri: `${process.env.SERVICE_ENDPOINT}/oauth/authorise`,
+  })
+  return new URL(`${process.env.SLACK_OAUTH_AUTHORISATION_PAGE}?${qs}`).href
+}
+
+export const getAccessTokenFromCode = (code) => {
+  const data = {
+    client_id: process.env.SLACK_CLIENT_ID,
+    client_secret: process.env.SLACK_CLIENT_SECRET,
+    redirect_uri: `${process.env.SERVICE_ENDPOINT}/oauth/authorise`,
+    code,
+  }
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  return axios.post('https://slack.com/api/oauth.access', stringify(data), config)
+    .then(response => response.data)
+    .then((data) => {
+      console.info(`Got response from Slack authorisation request: ${JSON.stringify(data)}`)
+      return data.ok ? data.access_token : null
     })
-    return new URL(`${process.env.SLACK_OAUTH_AUTHORISATION_PAGE}?${qs}`).href
+    .catch((error) => {
+      console.error(`Error from Slack authorisation request: ${error}`)
+      return null
+    })
 }
--- src/handler.ts
+++ src/handler.ts
@@ -1,22 +1,43 @@
 import { parse } from 'qs'
+import axios from 'axios'
 
 import { IHandlerResponse } from './common/types'
 import { formatJson } from './functions/format'
-import { getDirectInstallUrl } from './functions/oauth'
+import { getDirectInstallUrl, getAccessTokenFromCode } from './functions/oauth'
 
 export const slashCommand = async (event, context): Promise<IHandlerResponse> => {
-    const body = parse(event.body)
-    const message = formatJson(body)
+  const body = parse(event.body)
+  const message = formatJson(body)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(message)
+  }
+}
+
+export const authorise = async (event, content): Promise<IHandlerResponse> => {
+  const code = event.queryStringParameters.code
+  console.info(`Authorising from code: ${code}`)
+
+  const accessToken = await getAccessTokenFromCode(code)
+  if (!accessToken) {
     return {
-        statusCode: 200,
-        body: JSON.stringify(message)
+      statusCode: 200,
+      body: JSON.stringify({ error: `There was a problem authorising with Slack: could not get access token` })
     }
+  }
+  return {
+    statusCode: 302,
+    headers: { location: process.env.FMT_SUCCESS_LANDING_PAGE },
+    body: null
+  }
 }
 
 export const directInstall = async (event, context): Promise<IHandlerResponse> => {
-    return {
-        statusCode: 302,
-        headers: { location: getDirectInstallUrl() },
-        body: null
-    }
+  const directInstallUrl = getDirectInstallUrl()
+  console.info(`redirecting to ${directInstallUrl}`)
+  return {
+    statusCode: 302,
+    headers: { location: directInstallUrl },
+    body: null
+  }
 }
